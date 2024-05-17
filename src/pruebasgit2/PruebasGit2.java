7f81e4b7be94fad62c9e2cbf0305671d8f40cca8
David Duque <davduquediaz@gmail.com>
2024-05-17 22:04:46 +0200

    segundo commit

--- src/pruebasgit2/PruebasGit2.java
+++ src/pruebasgit2/PruebasGit2.java
@@ -2,6 +2,7 @@ package pruebasgit2;
 public class PruebasGit2 {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adiós mundo");
     }
     
 }
