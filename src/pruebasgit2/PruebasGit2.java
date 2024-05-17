f0737095fa4b49faabc188bd60eb0e052fcd71f1
Dummy <dummy@dummy.com>
2024-05-17 22:40:05 +0200

    mi primer commit desde Linux

--- src/pruebasgit2/PruebasGit2.java
+++ src/pruebasgit2/PruebasGit2.java
@@ -3,6 +3,7 @@ public class PruebasGit2 {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Adiós mundo");
+        System.out.println("Estoy trabajando desde Linux");
     }
     
 }
