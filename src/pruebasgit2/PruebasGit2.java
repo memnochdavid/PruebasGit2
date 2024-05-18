72c16d8a3607cbd564deb78c53f816720a166e4c
Dummy <dummy@dummy.com>
2024-05-18 19:15:22 +0200

    primer commit del fork

--- src/pruebasgit2/PruebasGit2.java
+++ src/pruebasgit2/PruebasGit2.java
@@ -3,8 +3,7 @@ public class PruebasGit2 {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Adiós mundo");
-        System.out.println("Estoy trabajando desde Linux");
-        
+        System.out.println("Estoy trabajando desde Linux");        
         System.out.println("Estos cambios son parte de mi FORK");
     }
     
